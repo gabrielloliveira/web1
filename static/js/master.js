6120a2d4193b2bbec9a02472c52bc126402e12b5
Deswtyny <pedroazevedo141@gmail.com>
2019-11-26 14:45:48 -0300

    Projeto Finalizado

--- static/js/master.js
+++ static/js/master.js
@@ -11,3 +11,25 @@ function getCookie() {
         document.getElementById('user-name').innerHTML = user[1]
     }
 }
+
+function troca_imagem(){
+    const inputs = document.getElementsByName('sexo')
+
+    for(let i = 0; i < inputs.length; i++){
+        inputs[i].addEventListener('click', ()=>{
+            let input_masculino = document.getElementById('sexo-masculino')
+            let input_feminino = document.getElementById('sexo-feminino')
+            let div_masculino = document.getElementById('imagem-sexo-masculino')
+            let div_feminino = document.getElementById('imagem-sexo-feminino')
+
+            if(input_masculino.checked){
+                div_feminino.setAttribute('style', 'display: none')
+                div_masculino.setAttribute('style', 'display: absolute')
+            }
+            if(input_feminino.checked){
+                div_masculino.setAttribute('style', 'display: none')
+                div_feminino.setAttribute('style', 'display: absolute')
+            }
+        })
+    }
+}
